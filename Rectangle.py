3514560ebeb568b354b839e28f69399b3b2dcec7
GracyAurora11 <gracy.aurora11@gmail.com>
2025-07-14 16:34:52 +0100

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,8 +1,22 @@
-import pgzrun
+import pgzrun,random
 WIDTH=800
 HEIGHT=650
 #main function
 def draw():
-    rec=Rect((200,200),(200,300))
-    screen.draw.rect(rec,(135,12,223))
+    r=255
+    g=0
+    b=random.randint(0,255)
+    hei=HEIGHT
+    wid=WIDTH - 280
+    for i in range(40):
+
+        rec=Rect((0,0),(wid,hei))
+        rec.center=(400,325)
+        screen.draw.rect(rec,(r,g,b))
+        hei -=10
+        wid +=10
+        g+=5
+        r-=5
+def update():
+    pass
 pgzrun.go()
