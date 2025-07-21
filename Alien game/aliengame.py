a1de7501668305e5fb1067bd7bb01eeb9a47fb1d
GracyAurora11 <gracy.aurora11@gmail.com>
2025-07-21 16:46:16 +0100

    Update aliengame.py

--- Alien game/aliengame.py
+++ Alien game/aliengame.py
@@ -3,8 +3,25 @@ import random
 TITLE = 'Alien Game'
 WIDTH=800
 HEIGHT=650
+mes=''
 #creating alien actor
 act1=Actor('alien.png')
 def draw():
+    screen.clear()
+    screen.fill('blue')
     act1.draw()
+    screen.draw.text(mes,(400,550),fontsize=30)
+    screen.draw.text('The alien game',(300,50),fontsize=40)
+def alien():
+    act1.x=random.randint(50,750)
+    act1.y=random.randint(50,600)
+def on_mouse_down(pos):
+    global mes
+    if act1.collidepoint(pos):
+        alien()
+        mes='Good job!'
+    else:
+        mes='Good try!'
+def update():
+    pass
 pgzrun.go()
