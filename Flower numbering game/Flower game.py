c4173c0830528bb3f0d1d72035077dc7dbcead81
GracyAurora11 <gracy.aurora11@gmail.com>
2025-09-08 16:37:17 +0100

    Update Flower game.py

--- Flower numbering game/Flower game.py
+++ Flower numbering game/Flower game.py
@@ -1,4 +1,5 @@
-import random, pgzrun, time
+import random, pgzrun
+from time import time
 WIDTH=700
 HEIGHT=600
 #game variables
@@ -10,11 +11,12 @@ flc=0
 tnf=8
 #function for creating flowers
 def ctf():
+    global sat
     for i in range(tnf):
         flo=Actor('flower.png')
         flo.pos=random.randint(50,650),random.randint(50,550)
         lof.append(flo)
-    sat=time.time()
+    sat=time()
 def draw():
     global sat,tot
     screen.blit('sunset field.jpg',(0,0))
@@ -27,9 +29,13 @@ def draw():
         screen.draw.line(l[0],l[1],color='white')
     #displaying timer
     if flc<tnf:
-        tot=time.time()-sat
+        tot=time()-sat
+        tot=int(tot)
         #tot=round(tot,1)
-        screen.draw.text(str(round(tot,1)),(25,25),color='blue')
+        #screen.draw.text(f'{tot:.1f}',(25,25),color='blue',fontsize=50)
+        screen.draw.text(str(tot),(25,25),color='blue',fontsize=50)
+    else:
+        screen.draw.text(str(tot),(25,25),color='blue',fontsize=50)
 def update():
     pass
 def on_mouse_down(pos):
