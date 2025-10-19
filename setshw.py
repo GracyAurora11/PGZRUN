bfc419768deb7c39e323baa000211e55f61433fb
GracyAurora11 <gracy.aurora11@gmail.com>
2025-10-19 16:10:37 +0100

    Add files via upload

---
+++ setshw.py
@@ -0,0 +1,7 @@
+maths = {'gracy','amelia','sophia','shreya','amber','kruthi','cara','freya'}
+english = {'gracy','keira','amber','mary','joseph','thomas','freya','cole','alex'}
+print((maths).union(english))
+print(maths.intersection(english))
+print(maths.difference(english))
+print(english.difference(maths))
+print(maths.symmetric_difference(english))
---
+++ tupleshw.py
@@ -0,0 +1,13 @@
+colors = ('orange','indigo','light green','purple','magenta','pink','white')
+print(colors)
+print(type(colors))
+print(len(colors))
+print(colors[1])
+
+#assigning multiple variables
+name,age,food,grade,country = ('Amy','15','Dumplings','11','Scotland')
+print('My name is ',name)
+print('I am',age,'years old')
+print('My favourite food is',food)
+print('My grade is ',grade)
+print('I am from',country)
