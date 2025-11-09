2e4955406dc602e06314a6ca6daf3174c15f7d47
GracyAurora11 <gracy.aurora11@gmail.com>
2025-11-09 16:57:49 +0000

    Update batandball.py

--- Batandball/batandball.py
+++ Batandball/batandball.py
@@ -1,11 +1,11 @@
 import random, pgzrun, pygame
-speedx=1
-speedy=1
+speedx=2
+speedy=2
 WIDTH=500
 HEIGHT=490
 #X and Y coordinates, Width and height numbers
 #Squares and rectangles code is same just with different lengths
-
+score=0
 bat=Rect((150,475),(75,50))
 ball=Rect((235,240),(35,35))
 def draw():
@@ -14,18 +14,23 @@ def draw():
     screen.fill('dark green')
     screen.draw.filled_rect(bat,'white')
     screen.draw.filled_rect(ball,'yellow')
-
+    screen.draw.text('Score:'+str(score),(50,50),color='white')
 def update():
-    global speedx, speedy
+    global speedx, speedy,score
     ball.x += speedx
     ball.y += speedy
     if ball.right>WIDTH or ball.left<0:
         speedx = -speedx
+#speed of the ball, if the ball goes out of the screen it has to pull it back in
     if ball.top<0 or ball.colliderect(bat):
         speedy = -speedy
+        score+=1
+    if ball.bottom>HEIGHT:
+        exit()
+#when the ball goes out of the screen, the game ends
     if keyboard.right:
-        bat.x += 2
+        bat.x += 3
     if keyboard.left:
-        bat.x -= 2
-
+        bat.x -= 3
+#the speed of the bat
 pgzrun.go()
