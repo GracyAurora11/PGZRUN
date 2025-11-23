8ede123fa1dfead134c65b6af66d04acfe7600fd
GracyAurora11 <gracy.aurora11@gmail.com>
2025-11-23 17:00:32 +0000

    Add files via upload

---
+++ Recycle Marathon/recycle.py
@@ -0,0 +1,10 @@
+import pgzrun,random
+WIDTH=800
+HEIGHT=800
+plastic=['battery.png','chipbag.png','plasticbag.png','waterbottle.png']
+
+def draw():
+    screen.clear()
+    screen.blit('background.png',(0,0))
+
+pgzrun.go()
