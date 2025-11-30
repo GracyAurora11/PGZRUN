4bfb4210e1ff7530d7771ce921b7e004763a59d7
GracyAurora11 <gracy.aurora11@gmail.com>
2025-11-30 16:56:03 +0000

    Add files via upload

---
+++ healthy diet game/healthy.py
@@ -0,0 +1,10 @@
+import pgzrun,random
+WIDTH=800
+HEIGHT=800
+unhealthy=['burger.jpg','chips.jpg','chocolate.jpg','pizza.jpg']
+
+def draw():
+    screen.clear()
+    screen.blit('tablebg',(0,0))
+
+pgzrun.go()
